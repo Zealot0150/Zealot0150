ff9bbc8d2d5c852b55df8c1ee60ff74b627b4b9c
Zealot0150 <93116858+Zealot0150@users.noreply.github.com>
2021-10-25 09:58:29 +0200

    Add files via upload

---
+++ FTicTackToe.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace TicTacToe
+{
+    partial class FTicTacToe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 370);
+            this.panel1.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button9.Location = new System.Drawing.Point(233, 238);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button8.Location = new System.Drawing.Point(127, 238);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button7.Location = new System.Drawing.Point(21, 238);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button6.Location = new System.Drawing.Point(233, 132);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button5.Location = new System.Drawing.Point(127, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button4.Location = new System.Drawing.Point(21, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button3.Location = new System.Drawing.Point(233, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button2.Location = new System.Drawing.Point(127, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Location = new System.Drawing.Point(21, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // FTicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(390, 405);
+            this.Controls.Add(this.panel1);
+            this.Name = "FTicTacToe";
+            this.Text = "3 i rad";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FTicTackToe.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+
+    public partial class FTicTacToe : Form
+    {
+        string player1 = "X";
+        string player2 = "O";
+        bool IsPlayer1 = true;
+        public FTicTacToe()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            // Welcome message
+            MessageBox.Show("Hej, välkommen till 3 i rad, må bästa person vinna\n Den första personen börjar med X, den andra får O, klicka den ruta ni vill ha");
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            if (changeStatus((Button)sender, IsPlayer1 ? player1 : player2))
+            {
+                if (CheckWin(IsPlayer1 ? player1 : player2))
+                {
+                    MessageBox.Show("Spelaren med " + player1 + " vann");
+                    ClearButtons();
+                  }
+                else
+                    IsPlayer1 = !IsPlayer1;
+                }
+            if (EndOfGame())
+                MessageBox.Show("Spelet slut, slutade oavgjort");
+        }
+        private void ClearButtons()
+        {
+            button1.Text =
+                button2.Text =
+                button3.Text =
+                button4.Text =
+                button5.Text =
+                button6.Text =
+                button7.Text =
+                button8.Text =
+                button9.Text = "";
+            IsPlayer1 = true;
+
+        }
+
+
+        private bool changeStatus(Button b, string player)
+        {
+            if (b.Text == "")
+            {
+                b.Text = player;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        private bool CheckWin(string player)
+        {
+            // Horisontellt
+            if (
+                ((button1.Text == player) & (button2.Text == player) & (button3.Text == player))
+                 )
+                return true;
+
+            if (
+                ((button4.Text == player) & (button5.Text == player) & (button6.Text == player))
+                 )
+                return true;
+
+            if (
+                ((button7.Text == player) & (button8.Text == player) & (button9.Text == player))
+                 )
+                return true;
+
+            // Vågrätt
+            if (
+                ((button1.Text == player) & (button4.Text == player) & (button7.Text == player))
+                 )
+                return true;
+
+            if (
+                ((button2.Text == player) & (button5.Text == player) & (button8.Text == player))
+                 )
+                return true;
+
+
+            if (
+                ((button3.Text == player) & (button6.Text == player) & (button9.Text == player))
+                 )
+                return true;
+
+            // Dia
+            if (
+                ((button1.Text == player) & (button5.Text == player) & (button9.Text == player))
+                 )
+                return true;
+            if (
+                ((button3.Text == player) & (button5.Text == player) & (button7.Text == player))
+                 )
+                return true;
+
+
+            return false;
+        }
+
+        private bool EndOfGame()
+        {
+            if ((button1.Text != "")
+                &
+                (button2.Text != "")
+                &
+                (button3.Text != "")
+                &
+                (button4.Text != "")
+                &
+                (button5.Text != "")
+                &
+                (button6.Text != "")
+                &
+                (button7.Text != "")
+                &
+                (button8.Text != "")
+                &
+                (button9.Text != ""))
+                return true;
+            return false;
+
+        }
+    }
+}
